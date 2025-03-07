1f8cb0f42e84198d1e240e5f568ef67de137a078
7onyx7 <170680705+7onyx7@users.noreply.github.com>
2025-03-07 13:42:08 -0600

    Renamed project to Wisdom

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes import auth_routes, reminder_routes
 
-app = FastAPI(title="AI Assistant Backend", version="0.1")
+app = FastAPI(title="Wisdom", description="secret", version="0.1")
 
 # Include Routes
 app.include_router(auth_routes.router)
